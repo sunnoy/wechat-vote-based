227552e9aa868f7de2faa6f2a0067e7a0bcc3c80
sunnoy <tzhsz.123@gmail.com>
2017-08-07 13:21:30 +0000

    nihao

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -13,14 +13,45 @@ class WechatController extends Controller
      */
     public function serve()
     {
-       // Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $wechat = app('wechat');
         $wechat->server->setMessageHandler(function($message){
-            return "欢迎关注德阳水质监测站投票平台！";
+            
+            switch ($message->MsgType) {
+        case 'event':
+            return '收到事件消息';
+            break;
+        case 'text':
+            if $message->Content == "你好"
+         ｛
+                     return '你也好';
+
+            ｝
+            
+            break;
+        case 'image':
+            return '收到图片消息';
+            break;
+        case 'voice':
+            return '收到语音消息';
+            break;
+        case 'video':
+            return '收到视频消息';
+            break;
+        case 'location':
+            return '收到坐标消息';
+            break;
+        case 'link':
+            return '收到链接消息';
+            break;
+        // ... 其它消息
+        default:
+            return '收到其它消息';
+            break;
+    }
+    
         });
 
-       // Log::info('return response.');
 
         return $wechat->server->serve();
     }
