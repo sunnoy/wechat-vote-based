9618af5a415d17784ff8b35ac639e79b3d805906
sunnoy <tzhsz.123@gmail.com>
2017-08-07 15:04:14 +0000

    kd

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use EasyWeChat\Message\Image;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
+
 
 
 class WechatController extends Controller
@@ -33,7 +35,7 @@ class WechatController extends Controller
             return '你好' . $message->FromUserName;
             break;
         case 'image':
-            Storage::put('file.jpg', $message->PicUrl);
+            Storage::put($message->FromUserName, $message->PicUrl);
             return "这是个图片" . $message->PicUrl;
             break;
         case 'voice':
