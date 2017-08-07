6dbc57b7245323e0500c536d157a181029fbceb1
sunnoy <tzhsz.123@gmail.com>
2017-08-07 15:19:14 +0000

    hhs

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,8 +45,8 @@ return [
 
         'local' => [
             'driver' => 'local',
-            //'root' => storage_path('app'),
-            'root' => storage_path('vote/'),
+            'root' => storage_path('app'),
+            //'root' => storage_path('vote/'),
         ],
 
         'public' => [
