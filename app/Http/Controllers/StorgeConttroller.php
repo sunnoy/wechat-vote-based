58d41b955bbcf4cc7f0f55e7b057f9bfd87b9081
sunnoy <ttzhsz@163.com>
2017-08-08 14:56:35 +0800

    ss

--- app/Http/Controllers/StorgeConttroller.php
+++ app/Http/Controllers/StorgeConttroller.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class StorgeConttroller extends Controller
 {
     public function SaveFile()
     {
-
+        //Storage::
     }
 }
