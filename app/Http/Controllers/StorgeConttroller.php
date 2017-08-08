1284fcb8dda9f502509d0bd6d808f7eeca13996d
sunnoy <ttzhsz@163.com>
2017-08-08 17:26:10 +0800

    zip

--- app/Http/Controllers/StorgeConttroller.php
+++ app/Http/Controllers/StorgeConttroller.php
@@ -4,30 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
+
 
 class StorgeConttroller extends Controller
 {
     public function SaveFile()
     {
-        $url ="http://mmbiz.qpic.cn/mmbiz_jpg/WxulJpJqk5MIRrkiaCS0QKb7sZllwZFvsqFpMcIZ8I7aOZWM9icd4rZyIQKuwibgqJzte0NXibvwGI4Ou2ibeu0UEDw/0";
-
-
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-
-        $file = curl_exec($ch);
-
-        curl_close($ch);
-
-
-
+        $file = '';
         $fileSave = Storage::disk('vote')->put(date("m-d-h-i-s") . ".jpg", $file);
         if ($fileSave) {
             return "Congratulations ! image saved success ! ";
