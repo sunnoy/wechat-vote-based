aa9ac2b853b78f7d3d4438fecc7343227a0a1023
sunnoy <ttzhsz@163.com>
2017-08-08 10:46:32 +0800

    linux

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use EasyWeChat\Message\Image;
 
 
 
@@ -19,10 +18,6 @@ class WechatController extends Controller
     {
 
         $wechat = app('wechat');
-        
-        
-        
-        
         $wechat->server->setMessageHandler(function($message){
             
           switch ($message->MsgType) {
---
+++ public/hook.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lr
+ * Date: 8/8/17
+ * Time: 10:38 AM
+ */
