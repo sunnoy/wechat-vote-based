ddbb25658a5ae1eb5f20ee73d4df58fcd794ea23
sunnoy <ttzhsz@163.com>
2017-08-08 15:46:54 +0800

    ss

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use EasyWeChat\Message\Text;
+use Illuminate\Support\Facades\Storage;
+
 
 
 class WechatController extends Controller
