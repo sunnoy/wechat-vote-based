7b7e0b6b3a5cb880059b041f506530fa48d2b933
sunnoy <ttzhsz@163.com>
2017-08-08 10:55:38 +0800

    tuling

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use EasyWeChat\Message\Text;
+
 
 
 
@@ -25,7 +27,15 @@ class WechatController extends Controller
             return '收到事件消息';
             break;
         case 'text':
-            return '你好' . $message->FromUserName;
+            $content = $message->Content;
+            //发送到图灵机器人接口
+            $url = "http://www.tuling123.com/openapi/api?key=f7b6e44c70ea46f3972d95e7bd044789&info=".$content;
+            //获取图灵机器人返回的内容
+            $content = file_get_contents($url);
+            //对内容json解码
+            $content = json_decode($content);
+            //把内容发给用户
+            return new Text(['content' => $content->text]);
             break;
         case 'image':
             return "这是个图片" . $message->PicUrl;
