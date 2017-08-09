766a12d8b08b1f4c650fbfe2b483b5f0080aa719
sunnoy <ttzhsz@163.com>
2017-08-09 08:02:25 +0800

    emai

--- app/Http/Controllers/StorgeConttroller.php
+++ app/Http/Controllers/StorgeConttroller.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 
 class StorgeConttroller extends Controller
 {
     public function SaveFile()
     {
-       exec("cd /var/www/we/storage/app/vote && zip -r ShuiZhiJianCeZhongXin.zip ./*");
+        exec("cd /var/www/we/storage/app/vote && zip -r ShuiZhiJianCeZhongXin.zip ./*");
+    }
+
+    public function sendEmail()
+    {
+        //popSTMP:phdedpoymmtigdea
+        //IMAPSTMP:rujnmulrolrbhiee
+
+
     }
 }
