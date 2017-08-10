43facb739aa75dd44240186a5c2549debc3c1747
sunnoy <ttzhsz@163.com>
2017-08-10 17:16:37 +0800

    tuling

--- app/Http/Controllers/StorgeConttroller.php
+++ app/Http/Controllers/StorgeConttroller.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\voteSendEmail;
 use GuzzleHttp\Client;
-
-
+use Illuminate\Support\Facades\Storage;
 
 
 class StorgeConttroller extends Controller
@@ -32,21 +31,8 @@ class StorgeConttroller extends Controller
     }
 
     public function test(){
-        $urlcon="德阳";
-        $client = new Client(); //GuzzleHttp\Client
-        $result = $client->request('POST','http://www.tuling123.com/openapi/api', [
-            'form_params' => [
-                'key' => 'f7b6e44c70ea46f3972d95e7bd044789',
-                'info' => $urlcon,
-                'userid' => '123',
-            ]
-        ]);
-       $content = $result->getBody()->getContents();
-        $content = json_decode($content);
-
-        //var_dump($result->getBody()->getContents());
-        return $content->text;
-
+        $fileNum = Storage::disk('vote')->files();
+        echo count($fileNum);
     }
 
 
