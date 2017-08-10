7b9eee3c0b2ef69cf005a8b472dfc64b9426220b
sunnoy <ttzhsz@163.com>
2017-08-10 15:31:07 +0800

    tuling

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -84,7 +84,7 @@ class WechatController extends Controller
                     $text3 = new Text(['content' => '您好！overtrue。']);
 
 
-                    return $text1;
+                    return $text1.$text2;
                     break;
                 case 'video':
                     return '收到视频消息';
