3ab920ab187d15e378d442b08a2e0a3750c17c2c
sunnoy <ttzhsz@163.com>
2017-08-13 10:43:06 +0800

    fix email vies

--- resources/views/emails/email.blade.php
+++ resources/views/emails/email.blade.php
@@ -1,7 +1,6 @@
 <body>
 <div align="center">
     <h1>水质监测站投票</h1>
-    <h3>日期: <span style="font-weight:bold">{{  date("m-d") }}</span></h3>
     <h3>投票数为: <span style="font-style: italic;color: red;font-weight:bold;font-size: xx-large">{{ $num-1 }}</span>张票</h3>
 
     <img src="{{ $message->embed(@storage_path('app/vote.png')) }}">
