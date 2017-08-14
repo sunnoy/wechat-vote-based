ab98b753e0c84ccc4846408462dc256fa8718de8
sunnoy <ttzhsz@163.com>
2017-08-15 07:52:58 +0800

    schedule fix bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,19 +34,16 @@ class Kernel extends ConsoleKernel
         $schedule->command('zipFiles')
             ->timezone('Asia/Shanghai')
             ->dailyAt('12:45');
-            //->dailyAt('15:40');
 
         //send emails
         $schedule->command('sendEmails')
             ->timezone('Asia/Shanghai')
             ->dailyAt('13:30');
-            //->dailyAt('15:42');
 
         //delete files
         $schedule->command('deleteFiles')
             ->timezone('Asia/Shanghai')
-            //->dailyAt('23:55');
-            ->dailyAt('15:45');
+            ->dailyAt('23:50');
     }
 
     /**
