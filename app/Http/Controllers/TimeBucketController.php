2297ed4188cd01ff6d7d3e6cf7f32c4ae96fd971
sunnoy <ttzhsz@163.com>
2017-08-18 16:33:26 +0800

    strtotime

--- app/Http/Controllers/TimeBucketController.php
+++ app/Http/Controllers/TimeBucketController.php
@@ -2,15 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use EasyWeChat\Foundation\Application;
+use EasyWeChat\Message\Text;
 use Illuminate\Http\Request;
 
 class TimeBucketController extends Controller
 {
 
+
     public function TimeBucker()
     {
-        //return date('Hi');
-        $we=app('hh',['jj'=>'jj']);
+
+        $sec = 1;
+        $min = $sec * 60;
+        $hou = $min * 60;
+        $day = $hou * 24;
+        $moth = $day * 30;
+        $year = $moth * 12;
+
+        echo 'nian ' . $year . '<br/>' . 'moth ' . $moth . '<br/>' . 'day ' . $day . '<br/>' . 'hour ' . $hou . '<br/>' . 'fen ' . $min;
+
+
+        echo '<br/>';
+        echo '<br/>';
+
+        echo 'now is  ' . date('Y-m-d');
+        echo '<br/>';
+
+        echo date('Y-m-d',strtotime('+1 month'));
+
 
 
     }
